66a1f0652f50ff9d55cb696711b2d1ea376f747c
dismyplay <dimadubrovin43@gmail.com>
2023-09-20 20:53:47 +0300

    first commit

--- lab0script.sh
+++ lab0script.sh
@@ -1,22 +1,26 @@
+
+#!/bin/sh
+#rm all previous files
+rm -rf ./lab0
+#part1
 cd ./lab0
-mkdir -m 305 electrode4
+mkdir electrode4
 cd ./electrode4
 echo "satk=3 sdef=4 spd=4" > timburr
-chmod 404 timburr
-mkdir -m 576 zigzagoon 
-mkdir -m 0537 toxicroak
+mkdir zigzagoon 
+mkdir toxicroak
 echo "Способности Overgrow\nChlorophyll Own Tempo" > petilil
-chmod 664 petilil
+chmod petilil
 echo "Способности  Overgrow Chlorophyll" >> serperior
 echo "Rivalry" >> serperior
-chmod 0060 serperior
+chmod serperior
 cd ..
-mkdir -m 755 granbull1
+mkdir -m granbull1
 cd granbull1
-mkdir -m 0513 drifloon
+mkdir -m drifloon
 echo "Тип покемона  GRASS DARK" >> cacturne
-chmod 404 cacturne
-mkdir -m u=rx,g=w,o=r shedinja
+chmod cacturne
+mkdir -m shedinja
 echo "weidth=7.1\nheight=24.0 atk=4 def=9" >> togetic
 echo "Развитые способности" >> tynamo
 echo "Lightningrod" >> tynamo
@@ -35,9 +39,36 @@ mkdir drifloon
 cd ..
 echo "Способности  Dark Art Illusion" >> zorua9
 echo "Prankster" >> zorua9
-
-
-
+#part2
+chmod 305 electrode4
+cd electrode4
+chmod 404 timburr
+chmod 576 zigzagoon
+chmod 537 toxicroak
+chmod 664 petilil
+chmod 060 serperior
+cd ..
+chmod 755 granbull1
+cd granbull1
+chmod 513 driftloon
+chmod 404 cacturne
+chmod u=rx, g=w, o=r shedinja
+chmod u=rw, g=w, o=w togetic 
+chmod 005 tynamo 
+cd ..
+chmod 444 magnezone3
+chmod 444 prinplup0
+chmod 777 weezing9
+cd weezing9
+chmod 537 cascoon
+chmod 550 roggenrola
+chmod 524 meditite
+chmod 620 voltorb
+chmod 375 staraptor
+chmod 500 driftloon
+cd .. 
+chmod 440 zorua9
+#part3
 
 
 
