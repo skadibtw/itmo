c731085908a5978023bb553f7acf0d16ff81a344
gev <gev@192.168.1.44>
2023-09-20 18:17:42 +0300

    first commit

---
+++ lab0script.sh
@@ -0,0 +1,43 @@
+cd ./lab0
+mkdir -m 305 electrode4
+cd ./electrode4
+echo "satk=3 sdef=4 spd=4" > timburr
+chmod 404 timburr
+mkdir -m 576 zigzagoon 
+mkdir -m 0537 toxicroak
+echo "Способности Overgrow\nChlorophyll Own Tempo" > petilil
+chmod 664 petilil
+echo "Способности  Overgrow Chlorophyll" >> serperior
+echo "Rivalry" >> serperior
+chmod 0060 serperior
+cd ..
+mkdir -m 755 granbull1
+cd granbull1
+mkdir -m 0513 drifloon
+echo "Тип покемона  GRASS DARK" >> cacturne
+chmod 404 cacturne
+mkdir -m u=rx,g=w,o=r shedinja
+echo "weidth=7.1\nheight=24.0 atk=4 def=9" >> togetic
+echo "Развитые способности" >> tynamo
+echo "Lightningrod" >> tynamo
+cd ..
+echo "Развитые способности" >> magnezone3
+echo "Analytic" >> magnezone
+echo "Способности  Growl Bubble Water Sport Peck Metal\nClaw Bubblebeam Bide Fury Attack Brine Whirlpool Drill Peck Hydro\Pump" >> prinplup0
+mkdir weezing9
+cd weezing9
+mkdir cascoon
+mkdir roggenrola
+mkdir meditite
+echo "Живет Urban" >> voltorb
+mkdir staraptor
+mkdir drifloon
+cd ..
+echo "Способности  Dark Art Illusion" >> zorua9
+echo "Prankster" >> zorua9
+
+
+
+
+
+
